e17c973908391b80ca2d5bf0937d803c29d260f6
ealos <55264267+ealos@users.noreply.github.com>
2022-01-11 17:54:58 +0100

    Add files via upload
    Piccolo esempio di composizione fra classi.
    La classe Figlio istanzia un oggetto della classe Padre.

---
+++ Figlio.cs
@@ -0,0 +1,24 @@
+﻿public class Figlio
+{
+    public Figlio() { }
+
+    Padre senior = new Padre();
+
+
+    public void MetodoDaEstendere2()
+    {
+        Console.WriteLine("Figlio, sono un altro metodo con la stessa signature.");
+    }
+
+    public static void Main(String[] args)
+    {
+        Padre father = new Padre();
+        Figlio son = new Figlio();
+
+        //son.MetodoDaEstendere1();        // non esiste qui e non può vedere (con questa sintassi) l'omonimo nella classe Padre 
+        son.MetodoDaEstendere2();          // dalla classe Figlio, non ricopre, non vede l'omonimo nella classe Padre
+       
+        son.senior.MetodoDaEstendere1();   // dalla classe Padre attraverso senior
+        son.senior.MetodoDaEstendere2();   // dalla classe Padre attraverso senior
+    }
+}
---
+++ Padre.cs
@@ -0,0 +1,14 @@
+﻿public class Padre
+{
+    public Padre() { }
+   
+    public void MetodoDaEstendere1()
+    {
+        Console.WriteLine("Padre, metodo da estendere 1.");
+    }
+
+    public void MetodoDaEstendere2()
+    {
+        Console.WriteLine("Padre, metodo da estendere  2.");
+    }
+}
