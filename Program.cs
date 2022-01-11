a4395296e5adcd9a904f5bc4a5c0b97268da0949
ealos <55264267+ealos@users.noreply.github.com>
2022-01-11 11:39:37 +0100

    Add files via upload
    Piccolo banale esempio base di ereditarietà fra una classe Padre e la relativa classe Figlio.
    La classe Padre offre tre metodi di cui uno privato, la classe Figlio ne sovrascrive uno solo.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿public class Padre
+{
+    public Padre() { }
+    private void MetodoDaNonEstendere()
+    {
+        Console.WriteLine("Padre, metodo da non estendere.");
+    }
+
+    public void MtodoDaEstendere1()
+    {
+        Console.WriteLine("Padre, metodo da estendere 1.");
+    }
+
+    public void MtodDaoEstendere2()
+    {
+        Console.WriteLine("Padre, metodo da estendere 2.");
+    }
+}
+
+public class Figlio : Padre
+{
+    public Figlio() { }
+
+    // metodoDaEstendere1() non viene sovrascritto
+
+
+    // sovrascrive, nasconde il metodo nella classe padre
+    public new void MtodDaoEstendere2()
+    {
+        Console.WriteLine("Figlio, metodo esteso 2.");
+    }
+}
+
+public class TheMain
+{
+    public static void Main(String[] args)
+    {
+        Padre father = new Padre();
+        Figlio son = new Figlio();
+
+        //father.metodoDaNonEstendere();   // Non è possibile richiamarlo è privato
+        father.MtodoDaEstendere1();        // dalla classe padre
+        father.MtodDaoEstendere2();        // dalla classe padre
+
+        //son.metodoDaNonEstendere();      // Non è possibile richiamarlo è privato
+        son.MtodoDaEstendere1();           // dalla classe padre 
+        son.MtodDaoEstendere2();           // dalla classe figlio 
+
+    }
+}
